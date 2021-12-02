32146b5498e9709e80a44f616068bedd2843488a
Jaime1998 <jaime.cuartas@correounivalle.edu.co>
2021-12-02 16:33:12 +0100

    add TrainGate

--- src/translation/Main/Main1Trace.java
+++ src/translation/Main/Main1Trace.java
@@ -15,6 +15,8 @@ public class Main1Trace {
                     "/c",
                     "\"C:\\Users\\Jaime\\OneDrive - correounivalle.edu.co\\Documentos\\uppaal-tron-1.5-win32\\tron.exe\"",
                     "C:\\Users\\Jaime\\Desktop\\Train-Gate-Controller\\carpetaMut\\tadSyncid8id4.xml",
+                    "-o",
+                    "output.txt",
                     "-Q",
                     "log",
                     "-I",
